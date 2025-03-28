452869ea95fe5b5accb9772eaddcf0d6251c3d5f
William Westlake <wwestlake@lagdaemon.com>
2025-03-28 06:14:32 -0400

    Added communication center

---
+++ Steamforge/CommunicationCenter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "CommunicationCenter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STEAMFORGE_API UCommunicationCenter : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+};
